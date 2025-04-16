26d013a6a8b9cbb2fb64f449c9d03ef560f806a0
samiraGoMyCode <saraelhilloui12@gmail.com>
2025-04-16 02:21:49 +0100

    V1.2

--- restau-test/src/Components/caissier/Header.jsx
+++ restau-test/src/Components/caissier/Header.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; 
 
 const HeaderCaissier = () => {
+    const navigate = useNavigate(); 
+  
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    localStorage.removeItem("email");
+    localStorage.removeItem("name");
+    localStorage.removeItem("userId");
+
+    navigate("/login");
+  };
   return (
     <div className="w-full flex justify-between items-center p-4 bg-gray-100 shadow">
       <h1 className="text-2xl font-bold text-gray-800">Espace Caissier</h1>
       <div className="flex gap-4 items-center">
         <span className="text-gray-600">Bienvenue, Samira</span>
-        <button className="text-red-500 hover:underline">Déconnexion</button>
+        <button className="text-red-500 hover:underline" onClick={handleLogout} >Déconnexion</button>
       </div>
     </div>
   );
